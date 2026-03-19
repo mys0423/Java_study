8af5829fa4e83ed34cb8d681d6974510cf85ca0a
mys0423 <mmmys2001@gmail.com>
2026-03-19 17:50:08 +0900

    feat: DB_day08

--- jdbc/src/vo/MemberVO.java
+++ jdbc/src/vo/MemberVO.java
@@ -3,97 +3,85 @@ package vo;
 import java.util.Objects;
 
 public class MemberVO {
-	//1. private
-	//2. 기본 생성자, 초기화 생성자
-	//3. getter, setter
-	//4. toString, hashCode, equals 재정의
-	private Long id;
-	private String memberEmail;
-	private String memberPassword;
-	private String memberAddress;
-	private String memberRecommenderEmail;
-	
-	public MemberVO() {;}
-
-	public MemberVO(String memberEmail, String memberPassword, String memberAddress) {
-		super();
-		this.memberEmail = memberEmail;
-		this.memberPassword = memberPassword;
-		this.memberAddress = memberAddress;
-	}
-
-	public MemberVO(Long id, String memberEmail, String memberPassword, String memberAddress,
-			String memberRecommenderEmail) {
-		super();
-		this.id = id;
-		this.memberEmail = memberEmail;
-		this.memberPassword = memberPassword;
-		this.memberAddress = memberAddress;
-		this.memberRecommenderEmail = memberRecommenderEmail;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getMemberEmail() {
-		return memberEmail;
-	}
-
-	public void setMemberEmail(String memberEmail) {
-		this.memberEmail = memberEmail;
-	}
-
-	public String getMemberPassword() {
-		return memberPassword;
-	}
-
-	public void setMemberPassword(String memberPassword) {
-		this.memberPassword = memberPassword;
-	}
-
-	public String getMemberAddress() {
-		return memberAddress;
-	}
-
-	public void setMemberAddress(String memberAddress) {
-		this.memberAddress = memberAddress;
-	}
-
-	public String getMemberRecommenderEmail() {
-		return memberRecommenderEmail;
-	}
-
-	public void setMemberRecommenderEmail(String memberRecommenderEmail) {
-		this.memberRecommenderEmail = memberRecommenderEmail;
-	}
-
-	@Override
-	public String toString() {
-		return "MemberVO [id=" + id + ", memberEmail=" + memberEmail + ", memberPassword=" + memberPassword
-				+ ", memberAddress=" + memberAddress + ", memberRecommenderEmail=" + memberRecommenderEmail + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		MemberVO other = (MemberVO) obj;
-		return Objects.equals(id, other.id);
-	}
-	
-	
+//   1. private
+//   2. 기본 생성자
+//   3. 초기화 생성자
+//   4. 개터새터
+//   5. to string 재정의
+//   6. hash, equals 재정의
+   private long id;
+   private String memberEmail;
+   private String memberPassword;
+   private String memberAddress;
+   private String memberRecommenderEmail;
+   
+   public MemberVO() {;}
+   public MemberVO(long id, String memberEmail, String memberPassword, String memberAddress,
+         String memberRecommenderEmail) {
+      this.id = id;
+      this.memberEmail = memberEmail;
+      this.memberPassword = memberPassword;
+      this.memberAddress = memberAddress;
+      this.memberRecommenderEmail = memberRecommenderEmail;
+   }
+   
+   public MemberVO(String memberEmail, String memberPassword, String memberAddress) {
+      this.memberEmail = memberEmail;
+      this.memberPassword = memberPassword;
+      this.memberAddress = memberAddress;
+   }
+   
+   public long getId() {
+      return id;
+   }
+   public void setId(long id) {
+      this.id = id;
+   }
+   public String getMemberEmail() {
+      return memberEmail;
+   }
+   public void setMemberEmail(String memberEmail) {
+      this.memberEmail = memberEmail;
+   }
+   public String getMemberPassword() {
+      return memberPassword;
+   }
+   public void setMemberPassword(String memberPassword) {
+      this.memberPassword = memberPassword;
+   }
+   public String getMemberAddress() {
+      return memberAddress;
+   }
+   public void setMemberAddress(String memberAddress) {
+      this.memberAddress = memberAddress;
+   }
+   public String getMemberRecommenderEmail() {
+      return memberRecommenderEmail;
+   }
+   public void setMemberRecommenderEmail(String memberRecommenderEmail) {
+      this.memberRecommenderEmail = memberRecommenderEmail;
+   }
+   
+   @Override
+   public String toString() {
+      return "TempMemberVO [id=" + id + ", memberEmail=" + memberEmail + ", memberPassword=" + memberPassword
+            + ", memberAddress=" + memberAddress + ", memberRecommenderEmail=" + memberRecommenderEmail + "]";
+   }
+   
+   @Override
+   public int hashCode() {
+      return Objects.hash(id);
+   }
+   
+   @Override
+   public boolean equals(Object obj) {
+      if (this == obj)
+         return true;
+      if (obj == null)
+         return false;
+      if (getClass() != obj.getClass())
+         return false;
+      MemberVO other = (MemberVO) obj;
+      return id == other.id;
+   }
 }
